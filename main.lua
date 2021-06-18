59988e6af76538e073b4f6389d9c939f9429ce87
gen40 <63437058+gen40@users.noreply.github.com>
2021-06-17 18:26:40 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,5 @@
---ugly code xD
-
-getgenv().autoclicker = true; --set to false if u want script to stop
+--pls don't make fun of the code
+getgenv().autoclicker = true --set to false if u want script to stop
 
 local AFK = game:GetService("ReplicatedStorage").Events:WaitForChild("AFK")
 -- game:GetService("Players").LocalPlayer.PlayerScripts.AFK:Destroy()
